56379b2b9830897ae511caf65734b7ee023a0698
Harshit Namdev <126095541+hunterhacker303@users.noreply.github.com>
2026-03-28 19:12:08 +0530

    Add files via upload

---
+++ iot_project.ino
@@ -0,0 +1,17 @@
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+
+// Try 0x27 or 0x3F if not working
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+
+void setup() {
+  Wire.begin(21, 22);   // ESP32 I2C pins
+  lcd.init();
+  lcd.backlight();
+
+  lcd.setCursor(0, 0);
+  lcd.print("Hello World");
+}
+
+void loop() {
+}
